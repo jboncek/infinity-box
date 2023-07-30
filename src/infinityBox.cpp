bb026ba41f6420ceb4dbdda32d04bc12bf03ba94
James Boncek <jboncek@gmail.com>
2023-07-30 13:37:56 -0500

    feat: prog interface

--- src/infinityBox.cpp
+++ src/infinityBox.cpp
@@ -31,11 +31,11 @@ void debug(const char * message, int someNumber){
   }
 }
 
-class BaseProgram {
+class ILedProgram {
   public:
     /// @brief Called when first detected as active
     /// @return void
-    void setup() {
+    void setup(){
       debug("base program setup app");
       // todo fix: get prog id below is always base = 0
       uint8_t idHue = map(getProgramId(), 0, MAX_PROGRAMS, 0, 255);
@@ -46,68 +46,9 @@ class BaseProgram {
       FastLED.show();
       delay(300);
     }
-  
-    /// @brief Used as program collection index and program change identifier. Override to be unique and > 0.  
-    /// @return int
-    int getProgramId(){
-      return 0;
-    }
 
     /// @brief Default behavior loops the led array and back to 0 automatically. Override if needed.  
     /// @return int
-    // int getNextIndex(int currentIndex){
-    //   if(currentIndex == NUM_LEDS){
-    //     return 0;
-    //   }
-    //   return currentIndex++;
-    // }
-
-    /// @brief Defines the hue to use for the next LED.  Override probably.
-    /// @return uint8_t
-    // uint8_t getNextHue(){
-    //   // override as needed
-    //   return 0;
-    // }
-
-    /// @brief Defines the lux to use for the next LED index iteration.  Lux cannot be controlled per LED. Override if needed.
-    /// @return int
-    int getNextLux(){
-      // override as needed
-      return MAX_LUX;
-    }
-
-    /// @brief The main function called from the loop.  Ideal behavior is to allow frequent main loops.
-    /// This provides responsive program control.  By default, the index will iterate all LEDs. 
-    /// Within main(), Call if(index = 0){  FastLED.show(); delay(200); } or similar to update the LEDs.
-    /// @return void
-    void main(int index, uint8_t hue, int lux){
-      // override in program concrete class
-    }
-
-    void setHue(int index, uint8_t hue){
-      _leds[index] = CHSV(hue, 255, 255);
-    }
-
-    void setRGB(int index, int r, int g, int b)
-    {
-      _leds[index] = CHSV(r, g, b);
-    }
-};
-
-class RotatingHue: public BaseProgram {
-  public:
-    int getProgramId(){
-      return 1;
-    }
-
-    uint8_t getNextHue(uint8_t currentHue){
-      if(currentHue == 254){
-        debug("reset hue loop");
-        return 0;
-      }
-      return currentHue + 1;
-    }
-
     int getNextIndex(int currentIndex){
       if(currentIndex == NUM_LEDS){
         debug("reset index loop");
@@ -117,13 +58,75 @@ class RotatingHue: public BaseProgram {
       return newIndex;
     }
 
+    /// @brief Defines the hue to use for the next LED.  Override probably.
+    /// @return uint8_t
+    uint8_t getNextHue(uint8_t currentHue){
+      if(currentHue == 254){
+        debug("reset hue loop");
+        return 0;
+      }
+      return currentHue + 1;
+    }
+
+    /// @brief Defines the lux to use for the next LED index iteration.  Lux cannot be controlled per LED. Override if needed.
+    /// @return int
+    int getNextLux(uint8_t currentLux){
+      return MAX_LUX;
+    }
+
+    /// @brief Helper wraps the LED library to set the hue for an LED
+    /// @return void
+    void setHue(int index, uint8_t hue) const {
+      _leds[index] = CHSV(hue, 255, 255);
+    }
+
+    /// @brief Helper wraps the LED library to set the RGB color for an LED
+    /// @return void
+    void setRgb(int index, int r, int g, int b)
+    {
+      _leds[index] = CHSV(r, g, b);
+    }
+
+    /// @brief Used as program collection index and program change identifier. Override to be unique and > 0.  
+    /// @return int
+    virtual int getProgramId() const = 0;
+
+    /// @brief The main function called from the loop.  Ideal behavior is to allow frequent main loops.
+    /// This provides responsive program control.  By default, the index will iterate all LEDs. 
+    /// Within main(), Call if(index = 0){  FastLED.show(); delay(200); } or similar to update the LEDs.
+    /// @return void
+    virtual void main(int index, uint8_t hue, uint8_t lux) = 0;
+  };
+
+class RotatingHue: public ILedProgram {
+  public:
+    int getProgramId() const {
+      return 1;
+    }
+    
     void main(int index, uint8_t hue, uint8_t lux)
     {
       if(true){
         FastLED.show();
       }
       delay(30);
-      setHue(index, hue);
+      ILedProgram::setHue(index, hue);
+    }
+};
+
+class Mimi: public ILedProgram {
+  public:
+    int getProgramId() const {
+      return 2;
+    }
+
+    void main(int index, uint8_t hue, uint8_t lux)
+    {
+      if(true){
+        FastLED.show();
+      }
+      delay(30);
+      ILedProgram::setHue(index, hue);
     }
 };
 
@@ -131,22 +134,22 @@ class Conductor {
   public:
     Conductor(){
       RotatingHue p1;
-      RotatingHue p2;
+      _p1 = p1;
+      Mimi p2;
+      _p2 = p2;
       RotatingHue p3;
+      _p3 = p3;
       RotatingHue p4;
-      RotatingHue p5;     
-      addProgram(p1);
-      addProgram(p2);
-      addProgram(p3);
-      addProgram(p4);
-      addProgram(p5);
+      _p4 = p4;
+      RotatingHue p5;
+      _p5 = p5;
     }
 
-    RotatingHue getActiveProgram(int currentProgramId){
+    ILedProgram& getActiveProgram(int currentProgramId){
       // todo: knob disabled for testing
       //int result = analogRead(KNOB_PIN);
-      //int activeProgramIndex = map(result, 0, 1023, 0, MAX_PROGRAMS);
-      RotatingHue activeProgram = getProgram(currentProgramId);
+      //int activeProgramIndex = map(result, 0, 1023, 1, MAX_PROGRAMS);
+      ILedProgram& activeProgram = getProgram(currentProgramId);
       if(activeProgram.getProgramId() != currentProgramId){
         resetState();
         activeProgram.setup();
@@ -155,18 +158,33 @@ class Conductor {
     }
 
   private:
-    RotatingHue _programs[MAX_PROGRAMS];
+    RotatingHue _p1;
+    Mimi _p2;
+    RotatingHue _p3;
+    RotatingHue _p4;
+    RotatingHue _p5;
 
-    void addProgram(RotatingHue program){
-      _programs[program.getProgramId()] = program;
-    }
-
-    RotatingHue getProgram(int index){
-      if(index < 1){
-        return _programs[1];
-      }
-      RotatingHue app = _programs[index];
-      return app;
+    ILedProgram& getProgram(int index){
+      switch (index)
+        {
+        case 1:
+          return _p1;
+          break;
+        case 2:
+          return _p2;
+          break;        
+        case 3:
+          return _p3;
+          break;
+        case 4:
+          return _p4;
+          break;      
+        case 5:
+          return _p5;
+          break;
+        default:
+          return _p1;
+        }
     }
 
     void resetState(){
@@ -182,7 +200,7 @@ class LedCommon {
     void fadeIn()
     {
         
-      for (int i = 0; i <= MAX_LUX; i++)
+      for (uint8_t i = _currentLux; i <= MAX_LUX; i++)
       {
         FastLED.setBrightness(i);
         FastLED.show();
@@ -192,7 +210,7 @@ class LedCommon {
 
     void fadeOut()
     {
-      for (int i = MAX_LUX; i >= 0; i--)
+      for (uint8_t i = _currentLux; i >= 0; i--)
       {
         delay(1);
         FastLED.setBrightness(i);
@@ -207,21 +225,25 @@ void setup()
 {
   FastLED.addLeds<WS2812B, LED_PIN, COLOR_ORDER>(_leds, NUM_LEDS);
   FastLED.setBrightness(0);
-  Serial.begin(9600);
+  if(_debugEnabled){
+    Serial.begin(9600);
+  }
   debug("setup complete");
   _conductor = Conductor();
 }
 
 void loop()
 {
-  RotatingHue program = _conductor.getActiveProgram(_currentProgramId);
+  ILedProgram& program = _conductor.getActiveProgram(_currentProgramId);
   _currentProgramId = program.getProgramId();
   int lastIndex = _currentIndex;
   uint8_t lastHue = _currentHue;
+  uint8_t lastLux = _currentLux;
   debug("loop index", _currentIndex);
   debug("loop hue", _currentHue);
+  debug("loop lux", _currentLux);
   _currentIndex = program.getNextIndex(lastIndex);
   _currentHue = program.getNextHue(lastHue);
-  _currentLux = program.getNextLux();
+  _currentLux = program.getNextLux(lastLux);
   program.main(_currentIndex, _currentHue, _currentLux);
 }
