3c71519f6c0609acd0c36de0e314a51948b3483d
jboncek <jboncek@gmail.com>
2023-09-30 13:44:07 -0500

    fix: breath responsive delay

--- lib/Breathe/src/Breathe.cpp
+++ lib/Breathe/src/Breathe.cpp
@@ -4,6 +4,9 @@
 
 #define KNOB_PIN A0
 
+int _delayCount;
+unsigned long _currentDelay;
+
 void Breathe::setup(){
     this->_common->debug("Breathe program setup fn");
     this->_currentHue = 0;
@@ -30,23 +33,31 @@ void Breathe::main(){
     this->_common->debug("min", appMin);
     this->_common->debug("max", appMax);
     this->_common->debug("speed", hue);
-    if(this->_currentLux == 0){
-        delay(this->_common->getRandomNumber(100,800));
-        this->_isGoingUp = true;
-    }
-    if(this->_currentLux == 255){
-        delay(this->_common->getRandomNumber(1000,2000));
-        this->_isGoingUp = false;
+    if(_currentDelay <  millis()){
+        if(_isGoingUp){
+            this->_currentLux++;
+        }else{
+            this->_currentLux--;
+        }
+        if(this->_currentLux == 0)
+        {
+            _currentDelay = this->_common->getRandomNumber(100,800) + millis();
+            this->_isGoingUp = true;
+        }
+        else if (this->_currentLux == 255) 
+        {
+            _currentDelay = this->_common->getRandomNumber(1000,2000) + millis();
+            this->_isGoingUp = false;
+        } else
+        {
+            _currentDelay = this->_common->getRandomNumber(0,5) + millis();
+        }
     }
     if(this->_isGoingUp){
-        this->_currentLux++;
         this->_common->setAllHue(hue, 255, this->_currentLux);
         this->_common->show();
-        delay(this->_common->getRandomNumber(0,5));
     }else{
-        this->_currentLux--;
         this->_common->setAllHue(hue, 255, this->_currentLux);
         this->_common->show();
-        delay(this->_common->getRandomNumber(0,5));
     }
 }
